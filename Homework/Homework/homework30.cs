a4be72f9672e28bc9a73ae4afc18ac5dd2ca75aa
zippaks <darkerblue15@gmail.com>
2019-03-15 23:29:18 +0200

    adding homework

---
+++ Homework/Homework/homework30.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int capacity = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (counter < capacity)
+            {
+                string search = Console.ReadLine();
+
+                if (search == book)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    break;
+                }
+                else if (search != book)
+                {
+                    counter++;
+                }
+
+
+            }
+            if (counter >= capacity)
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+
+            }
+
+
+        }
+    }
+}
