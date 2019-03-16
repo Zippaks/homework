18b8cf979931a1049a0ddc49f53c82dd645b6fa3
Zippaks <48570586+Zippaks@users.noreply.github.com>
2019-03-16 14:46:16 +0200

    Add files via upload

---
+++ Task5.cs
@@ -0,0 +1,43 @@
+﻿sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int size = a * b;
+            int piece = 0;
+
+            while (size > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "STOP")
+                {
+                    Console.WriteLine($"{size - piece} pieces are left.");
+                    break;
+
+                }
+                int parche = int.Parse(command);
+                piece += parche;
+                if (piece > size)
+                {
+                    Console.WriteLine($"No more cake left! You need {piece - size} pieces more.");
+                    break;
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
